780d4e346c8d5fc288350cb45fdae3419c676399
Jimen <hurjeesic@naver.com>
2019-11-08 21:26:05 +0900

    장고 실행 방법 추가 데이터(은어) 목록 확인 페이지 추가 및 css 파일 추가

--- patter/views.py
+++ patter/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Patter
 
 # Create your views here.
 def patter_list(request):
-    return render(request, 'patter/patter_list.html', {})
+    patters = Patter.objects.all()
+    return render(request, 'patter/patter_list.html', {'patters' : patters})
