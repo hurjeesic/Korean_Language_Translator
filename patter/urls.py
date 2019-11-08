07754504ec9cd672fca485f7f5b4e5a6a8ff689f
Jimen <hurjeesic@naver.com>
2019-11-08 22:20:22 +0900

    은어 목록 페이지를 위한 index 페이지 추가

--- patter/urls.py
+++ patter/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.patter_list, name='patter_list'),
+    path('', views.index, name='index'),
+    path('patter/', views.patter_list, name='patter_list'),
     path('patter/<str:pk>/', views.patter_detail, name='patter_detail')
 ]
--- patter/views.py
+++ patter/views.py
@@ -3,10 +3,15 @@ from django.utils import timezone
 from .models import Patter
 
 # Create your views here.
+def index(request):
+    return render(request, 'patter/index.html', {})
+
 def patter_list(request):
     patters = Patter.objects.all()
-    return render(request, 'patter/patter_list.html', {'patters' : patters})
+
+    return render(request, 'patter/patter_list.html', { 'patters' : patters })
 
 def patter_detail(request, pk):
     patter = get_object_or_404(Patter, pk=pk)
-    return render(request, 'patter/patter_detail.html', {'patter' : patter})
+
+    return render(request, 'patter/patter_detail.html', { 'patter' : patter })
